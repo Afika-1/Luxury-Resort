be94d49617a864916e59b4577365878a699bfd26
Afika <afikashwashwa@gmail.com>
2025-08-15 12:07:39 +0200

    vERCEL PATHWAY ADDED

--- luxury-resort/vite.config.js
+++ luxury-resort/vite.config.js
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
+
   ],
+  base: ProcessingInstruction.env.VITE_BASE_PATH||"//Luxury-Resort"
   
 })
